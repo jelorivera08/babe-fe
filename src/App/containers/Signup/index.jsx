41807bc7bd7e421dae49bf3e70deff4d5a26f496
jelorivera08 <jerald@virtualcontrol.io>
2020-04-28 13:44:26 +0800

    remove file upload add favicon

--- src/App/containers/Signup/index.jsx
+++ src/App/containers/Signup/index.jsx
@@ -10,6 +10,8 @@ import Header from "../../components/Header";
 import environment from "../../../environment";
 import axios from "axios";
 
+import { PROD_API } from "../../../constants";
+
 import { options, regionOptions } from "./constants";
 
 const mutation = graphql`
@@ -81,20 +83,21 @@ const SignUp = () => {
       variables: { ...values },
       onCompleted: (response, errors) => {
         if (response && response.userCreate && response.userCreate.username) {
-          const formData = new FormData();
-          formData.append("image", imgFile);
-          formData.append("username", credentials.username);
+          return setRegistrationSucess(true);
+          //   const formData = new FormData();
+          //   formData.append("image", imgFile);
+          //   formData.append("username", credentials.username);
 
-          axios
-            .post("http://localhost:4000/upload", formData, {})
-            .then(() => {
-              setRegistrationSucess(true);
-            })
-            .catch((err) => setError(err));
-        }
+          //   axios
+          //     .post(`${PROD_API}/upload`, formData, {})
+          //     .then(() => {
+          //       setRegistrationSucess(true);
+          //     })
+          //     .catch((err) => setError(err));
+          // }
 
-        if (errors) {
-          setError(errors[0].message);
+          // if (errors) {
+          //   setError(errors[0].message);
         }
       },
       onError: (err) => setError(err),
@@ -115,13 +118,13 @@ const SignUp = () => {
           <Card.Content>
             <Card.Header>Registration Success!</Card.Header>
             <Card.Description>
-              <div className="mb-1">
+              <div className='mb-1'>
                 Please wait until your account is reviewed by our staff.
               </div>
               <div> You will be contacted accordingly.</div>
             </Card.Description>
           </Card.Content>
-          <Card.Content extra className="flex justify-end">
+          <Card.Content extra className='flex justify-end'>
             <Button
               primary
               onClick={() => {
@@ -134,44 +137,44 @@ const SignUp = () => {
           </Card.Content>
         </Card>
       ) : (
-        <div className="w-full max-w-2xl">
+        <div className='w-full max-w-2xl'>
           <form
             onSubmit={(e) => {
               e.preventDefault();
               handleSubmit(credentials);
             }}
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
           >
-            <div className="flex">
-              <div className="mb-4 w-1/2 mr-2">
+            <div className='flex'>
+              <div className='mb-4 w-1/2 mr-2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="username"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='username'
                 >
                   Username
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  placeholder="Username"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='username'
+                  type='text'
+                  placeholder='Username'
                   value={credentials.username}
                   onChange={(e) =>
                     setCredentials({ ...credentials, username: e.target.value })
                   }
                 />
               </div>
-              <div className="mb-4 w-1/2 mr-2">
+              <div className='mb-4 w-1/2 mr-2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="photo"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='photo'
                 >
                   Upload your photo
                 </label>
                 <input
-                  className="appearance-none w-full py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline"
-                  name="userPhoto"
-                  type="file"
+                  className='appearance-none w-full py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline'
+                  name='userPhoto'
+                  type='file'
                   onChange={(e) => {
                     setImgFile(e.target.files[0]);
                   }}
@@ -179,19 +182,19 @@ const SignUp = () => {
               </div>
             </div>
 
-            <div className="flex">
-              <div className="mb-6 mr-2 w-1/2">
+            <div className='flex'>
+              <div className='mb-6 mr-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="username"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='username'
                 >
                   Password
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  type="password"
-                  placeholder="********"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='password'
+                  type='password'
+                  placeholder='********'
                   value={credentials.password}
                   onChange={(e) =>
                     setCredentials({ ...credentials, password: e.target.value })
@@ -199,18 +202,18 @@ const SignUp = () => {
                 />
               </div>
 
-              <div className="mb-4 ml-2 w-1/2">
+              <div className='mb-4 ml-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="username"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='username'
                 >
                   Confirm password
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="confirmPassword"
-                  type="password"
-                  placeholder="********"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='confirmPassword'
+                  type='password'
+                  placeholder='********'
                   value={credentials.confirmPassword}
                   onChange={(e) =>
                     setCredentials({
@@ -222,19 +225,19 @@ const SignUp = () => {
               </div>
             </div>
 
-            <div className="flex">
-              <div className="mb-4 mr-2 w-1/2">
+            <div className='flex'>
+              <div className='mb-4 mr-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="firstname"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='firstname'
                 >
                   First name
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="firstname"
-                  type="text"
-                  placeholder="Babe"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='firstname'
+                  type='text'
+                  placeholder='Babe'
                   value={credentials.firstName}
                   onChange={(e) =>
                     setCredentials({
@@ -245,18 +248,18 @@ const SignUp = () => {
                 />
               </div>
 
-              <div className="mb-4 ml-2 w-1/2">
+              <div className='mb-4 ml-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="surname"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='surname'
                 >
                   Surname
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="surname"
-                  type="text"
-                  placeholder="Dela Cruz"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='surname'
+                  type='text'
+                  placeholder='Dela Cruz'
                   value={credentials.surname}
                   onChange={(e) =>
                     setCredentials({ ...credentials, surname: e.target.value })
@@ -265,59 +268,59 @@ const SignUp = () => {
               </div>
             </div>
 
-            <div className="flex">
-              <div className="mb-4 w-1/2">
+            <div className='flex'>
+              <div className='mb-4 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="accountType"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='accountType'
                 >
                   Account type
                 </label>
 
                 <Select
-                  className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
                   onChange={(e, { value }) =>
                     setCredentials({ ...credentials, accountType: value })
                   }
                   value={credentials.accountType}
-                  placeholder="Account type"
+                  placeholder='Account type'
                   options={options}
                 />
               </div>
 
-              <div className="mb-4 ml-2 w-1/2">
+              <div className='mb-4 ml-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="region"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='region'
                 >
                   Region
                 </label>
 
                 <Select
-                  className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
                   onChange={(e, { value }) =>
                     setCredentials({ ...credentials, region: value })
                   }
                   value={credentials.region}
-                  placeholder="Region"
+                  placeholder='Region'
                   options={regionOptions}
                 />
               </div>
             </div>
 
-            <div className="flex">
-              <div className="mb-6 mr-2 w-1/2">
+            <div className='flex'>
+              <div className='mb-6 mr-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="username"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='username'
                 >
                   Facebook URL
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="facebookURL"
-                  type="text"
-                  placeholder="BabeDelaCruz"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='facebookURL'
+                  type='text'
+                  placeholder='BabeDelaCruz'
                   value={credentials.facebookURL}
                   onChange={(e) =>
                     setCredentials({
@@ -328,18 +331,18 @@ const SignUp = () => {
                 />
               </div>
 
-              <div className="mb-4 ml-2 w-1/2">
+              <div className='mb-4 ml-2 w-1/2'>
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="username"
+                  className='block text-gray-700 text-sm font-bold mb-2'
+                  htmlFor='username'
                 >
                   Instagram URL
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="instagramURL"
-                  type="text"
-                  placeholder="BabeDelaCruz"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  id='instagramURL'
+                  type='text'
+                  placeholder='BabeDelaCruz'
                   value={credentials.instagramURL}
                   onChange={(e) =>
                     setCredentials({
@@ -351,18 +354,18 @@ const SignUp = () => {
               </div>
             </div>
 
-            <div className="mb-6 mr-2 w-full">
+            <div className='mb-6 mr-2 w-full'>
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="username"
+                className='block text-gray-700 text-sm font-bold mb-2'
+                htmlFor='username'
               >
                 Description
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="facebookURL"
-                type="text"
-                placeholder="Brief description of yourself"
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                id='facebookURL'
+                type='text'
+                placeholder='Brief description of yourself'
                 value={credentials.description}
                 onChange={(e) => {
                   if (credentials.description.length >= 100) return null;
@@ -375,21 +378,23 @@ const SignUp = () => {
               />
             </div>
 
-            <div className="flex justify-between mt-4">
+            <div className='flex justify-between mt-4'>
               {error ? (
-                <p className="text-red-500 text-m italic">{error}</p>
+                <p className='text-red-500 text-m italic'>
+                  {JSON.stringify(error)}
+                </p>
               ) : (
                 <div />
               )}
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+                type='submit'
               >
                 Submit
               </button>
             </div>
           </form>
-          <p className="text-center text-gray-200 text-xs">
+          <p className='text-center text-gray-200 text-xs'>
             &copy;2020 Babe Inc. All rights reserved.
           </p>
         </div>

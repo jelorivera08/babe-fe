0e71982b5036f788851740653bb26edcd870767b
jelorivera08 <jerald@virtualcontrol.io>
2020-05-01 21:18:20 +0800

    hide about us for now

--- src/App/components/Header/index.jsx
+++ src/App/components/Header/index.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
-import logo1 from '../../../assets/images/logo1.PNG';
+import React from "react";
+import logo1 from "../../../assets/images/logo1.PNG";
 
 const HeaderContainer = ({ history }) => (
-  <div className="fixed w-full bg-transparent h-12 flex justify-between items-center pr-8 top-0">
+  <div className='fixed w-full bg-transparent h-12 flex justify-between items-center pr-8 top-0'>
     <div>
-      <img role="button" onClick={() => history.push('/')} className="h-12 ml-2 cursor-pointer" src={logo1} alt="logo1" />
+      <img
+        role='button'
+        onClick={() => history.push("/")}
+        className='h-12 ml-2 cursor-pointer'
+        src={logo1}
+        alt='logo1'
+      />
     </div>
 
-    <div className="flex ">
-      <div
+    <div className='flex '>
+      {/* <div
 
         role="button"
         tabIndex="0"
@@ -22,30 +28,30 @@ const HeaderContainer = ({ history }) => (
       >
         About us
 
-      </div>
+      </div> */}
       <div
-        role="button"
-        tabIndex="0"
-        onClick={() => { history.push('/resellers'); }}
-        className="hover:underline cursor-pointer mr-4"
+        role='button'
+        tabIndex='0'
+        onClick={() => {
+          history.push("/resellers");
+        }}
+        className='hover:underline cursor-pointer mr-4'
       >
         Resellers
-
       </div>
 
       <div
-        role="button"
-        tabIndex="0"
-        onClick={() => { history.push('/signup'); }}
-        className="hover:underline cursor-pointer"
+        role='button'
+        tabIndex='0'
+        onClick={() => {
+          history.push("/signup");
+        }}
+        className='hover:underline cursor-pointer'
       >
         Sign up
-
       </div>
     </div>
-
   </div>
 );
-
 
 export default HeaderContainer;
